e45b1c6dfda1d385780c00522db876aca5475e29
way0750 <bruce0750@gmail.com>
2015-10-29 20:22:28 -0700

    19 passed

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -60,7 +60,7 @@ var getGitHubProfile = function (user, callback) {
  });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -71,7 +71,7 @@ var generateRandomToken = function (callback) {
  });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
